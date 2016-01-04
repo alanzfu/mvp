f1edc3a5aa597e9f3eb6b1c274b59dfd1d4635f6
Hack Reactor Students <accounts@hackreactor.com>
2016-01-04 11:41:08 -0800

    Backend commit

---
+++ server/apiHandler.js
@@ -0,0 +1,10 @@
+var pg = require('pg');
+var Promise = require('bluebird');
+
+module.exports.technologies = function(req, res){
+  //query database
+}
+
+module.exports.companies = function(req, res){
+  //query database
+}
---
+++ server/routes.js
@@ -0,0 +1,6 @@
+var apiHandler = require('./apiHandler.js');
+
+module.exports = function(app, express) {
+  app.get('api/companies', apiHandler.companies);
+  app.post('api/companies/technologies', apiHandler.technologies);
+}

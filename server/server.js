8d29b4235a11009fbd57c5ee8bd749582dcd1738
Hack Reactor Students <accounts@hackreactor.com>
2016-01-04 22:04:40 -0800

    Adds angular via cdn

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParse = require('body-parser');
 var cors = require('cors');
+var path = require('path')
 
 
 var app = express();
@@ -12,6 +13,6 @@ require('./routes.js')(app, express);
 app.use(cors());
 
 
-app.use('/',express.static('./public'));
+app.use('/',express.static(path.join(__dirname,'/public')));
 
 app.listen(process.env.PORT || 3000);
